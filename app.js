a7625aca1be2f06893e9d7e67d3b6cba79235742
MikeNichols1371 <mnich13711@gmail.com>
2022-08-08 14:29:54 -0400

    Completed Exercises

---
+++ app.js
@@ -0,0 +1,57 @@
+//Exercise 1
+const firstName = "Mike";
+let lastName = "Nichols";
+var age = 25;
+let exercise1 =
+document.getElementById("E1");
+exercise1.innerHTML = `First Name: ${firstName} 
+Last Name: ${lastName} 
+ Age: ${age}`;
+
+//Exercise 2
+let test = "mike"
+lastName = "Nichols";
+age = 25;
+let exercise2 = document.getElementById("E2");
+exercise2.innerHTML= `First Name: ${test} Last Name: ${lastName} Age: ${age}`;
+
+//Exercise 3 
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let exercise3 = 
+document.getElementById("E3");
+exercise3.innerHTML = `Language: ${language} 
+Created Year: ${createdYear} 
+Case Sensitve: ${isCaseSensitive}`;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+let exercise4 = 
+document.getElementById("E4");
+exercise4.innerHTML = `Price: ${price}
+On Sale: ${isOnSale}
+Sale Percentage: ${salePercentage}
+Stock: ${stock}
+In Stock: ${inStock}
+Selected Size: ${selectedSize}`;
+
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+let exercise5 =
+document.getElementById("E5");
+ exercise5.innerHTML = `Title: ${title}
+Author: ${author}
+Page Count: ${pageCount}
+Bookmark: ${bookmark}
+Has Read: ${hasRead}`;
